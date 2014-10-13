f7ba4b2f0196922082b7afbc7d9fd1beeccb1bb2
Alyssa Ross <hi@alyssa.is>
2014-10-13 10:44:45 +0100

    Add tests

---
+++ test.rb
@@ -0,0 +1,16 @@
+require 'sass'
+require 'minitest/spec'
+require 'minitest/autorun'
+
+Sass.load_paths << File.dirname(__FILE__)
+
+Dir.glob("test/**/*.s{a,c}ss").each do |file|
+  describe file do
+    it "compiles correctly" do
+      expected = file.sub(/\.s[ac]ss/, '.css')
+      syntax = File.extname(file)[1..-1].to_sym
+      css = Sass.compile(open(file).read, syntax: syntax)
+      css.must_equal open(expected).read
+    end
+  end
+end
